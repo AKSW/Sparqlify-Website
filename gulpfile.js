b950e44c073550184eaa794ad724c0bd6afbe5f8
Simonjohanning <Simonjohanning@gmail.com>
2015-09-09 15:09:49 +0200

    clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,23 @@
 var gulp = require('gulp');
 var git = require('gulp-git');
+var bower = require('gulp-bower');
+var inject = require('gulp-inject');
 
 gulp.task('update', function(cb) {
   git.pull('origin', 'master', cb);
+});
+
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest('public/www/bower_components'))
+});
+
+gulp.task('index', function () {
+  var target = gulp.src('./public/_layout.ejs');
+  // It's not necessary to read the files (will speed up things), we're only after their paths: 
+  var sources = gulp.src(['./public/www/bower_components/**/*.js', './public/www/bower_components/**/*.css'], {read: false}, {relative : true});
+ 
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./public'));
 });
 
